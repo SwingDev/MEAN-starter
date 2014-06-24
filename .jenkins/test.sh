21148342db9cfeb0c2be49d56f60fe7bf95322df
Marcin Mincer <marcin.mincer@gmail.com>
2014-06-24 14:31:35 +0200

    Update test.sh

--- .jenkins/test.sh
+++ .jenkins/test.sh
@@ -4,4 +4,4 @@ export PATH=$PATH:/Users/jenkins/.npm/bin
 cd $WORKSPACE
 mocha --reporter tap --timeout 10000 --recursive build/server/test > $WORKSPACE/.jenkins/reports/mocha.tap
 mocha-casperjs --reporter=tap --recursive build/server/e2e_test/* > $WORKSPACE/.jenkins/reports/casper.tap
-istanbul cover _mocha -- --recursive build/server --timeout 5000
+istanbul cover _mocha -- --recursive build/server/test --timeout 5000
